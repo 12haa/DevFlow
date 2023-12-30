3b1c69cb2959d410378bfac6bbdea095190edc40
12haa <12hadoto@gmail.com>
2023-12-30 20:53:48 +0330

    Add MobileNav Component

--- app/components/shared/Navbar/MobileNav.tsx
+++ app/components/shared/Navbar/MobileNav.tsx
@@ -1,6 +1,8 @@
+"use client";
 import React from "react";
 import {
   Sheet,
+  SheetClose,
   SheetContent,
   SheetDescription,
   SheetHeader,
@@ -9,7 +11,47 @@ import {
 } from "@/components/ui/sheet";
 import Image from "next/image";
 import Link from "next/link";
+import { SignedOut } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import { sidebarLinks } from "@/app/constants";
+import { usePathname } from "next/navigation";
 
+const NavContent = () => {
+  const pathname = usePathname();
+  return (
+    <section className="flex flex-col h-full gap-6 pt-16  ">
+      {sidebarLinks.map((link) => {
+        const isActive =
+          (pathname.includes(link.route) && link.route.length > 1) ||
+          pathname === link.route;
+        return (
+          <SheetClose asChild key={link.route}>
+            <Link
+              key={link.route}
+              href={link.route}
+              className={`${
+                isActive
+                  ? "primary-gradient rounded-lg text-light-900"
+                  : "text-dark300_light900"
+              }flex items-center justify-start gap-4 bg-transparent `}
+            >
+              <Image
+                src={link.imgURL}
+                alt={link.label}
+                width={20}
+                height={20}
+                className={`${isActive ? "" : "invert-colors"}`}
+              />
+              <p className={`${isActive ? "base-bold" : "base-medium"}`}>
+                {link.label}
+              </p>
+            </Link>
+          </SheetClose>
+        );
+      })}
+    </section>
+  );
+};
 const MobileNav = () => {
   return (
     <Sheet>
@@ -37,7 +79,30 @@ const MobileNav = () => {
             Dev<span className="text-primary-500">Flow</span>
           </p>
         </Link>
-        <div>16 47</div>
+        <div>
+          <SheetClose asChild>
+            <NavContent />
+          </SheetClose>
+
+          <SignedOut>
+            <div className="flex flex-col gap-3 ">
+              <SheetClose asChild>
+                <Link href="/sign-in">
+                  <Button className="small-mediuim btn-secondary w-full min-h-[41px] rounded-lg px-4 py-3 shadow-none">
+                    <span className="primary-text-gradient">LogIn</span>
+                  </Button>
+                </Link>
+              </SheetClose>
+              <SheetClose asChild>
+                <Link href="/sign-up">
+                  <Button className="small-mediuim light-border-2 btn-tretiary w-full min-h-[41px] rounded-lg px-4 py-3 shadow-none text-dark400_light900">
+                    Sign Up
+                  </Button>
+                </Link>
+              </SheetClose>
+            </div>
+          </SignedOut>
+        </div>
       </SheetContent>
     </Sheet>
   );
