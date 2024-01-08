e23fb7939144bad796e7a06a7deb987334bbfa32
12haa <12hadoto@gmail.com>
2024-01-08 16:56:16 +0330

    Start LocalSearchbar

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,5 +1,29 @@
 import { auth, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import LocalSearchbar from "@/app/components/shared/Search/LocalSearchbar";
 
 export default function Home() {
-  return <div className="">Home</div>;
+  return (
+    <>
+      <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900 ">All Questions </h1>
+        <Link href="/ask-question" className="flex justify-end max-sm:w-full">
+          <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900 ">
+            Ask Question
+          </Button>
+        </Link>
+      </div>
+
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        <LocalSearchbar
+          route="/"
+          iconPositions="left"
+          imgSrc="/assets/icons/search.svg"
+          placeholder="Search for Questions"
+          otherClasses="flex-1"
+        />
+      </div>
+    </>
+  );
 }
